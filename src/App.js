8e05af2b3255946b75d525ec06c135f04bac04a6
David <gvdavid.tran@gmail.com>
2018-08-04 14:55:40 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Card from './Card'
+import { robots } from './robots'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      </div>
+    );
+  }
+}
+
+export default App;
