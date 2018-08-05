7dc2aa3a8a7157515f5b51e500deb9cd8ebe99a4
David Tran <gvdavid.tran@gmail.com>
2018-08-04 22:15:21 -0400

    added CardList component

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
   const { id, name, email } = props;
   return(
-    <div className='bg-light-green dib br3 ma2 grow bw2 shadow-5'>
+    <div className='tc bg-light-green dib br3 ma2 grow bw2 shadow-5'>
       <img alt='' src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
